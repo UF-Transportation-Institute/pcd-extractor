9e7f30931d8ad55a6ba63dbbaaed97cd6f24fe76
lebedev <ylebedev@ufl.edu>
2021-01-18 14:37:02 -0500

    fixed naming convesions

--- src/pcd_extractor_node.cpp
+++ src/pcd_extractor_node.cpp
@@ -59,7 +59,7 @@ void ObjectPCDExtractor::pointCloudCallback(const sensor_msgs::PointCloud2::Cons
     sprintf(path, "/media/lightning/Samsung_T3/Lidar/clusters/cluster%d.pcd", pcl_pc->header.seq);
     pcl::io::savePCDFile(path, *pcl_pc);
 
-    if(print_fps_)if(++frames>10){std::cerr<<"[object3d_detector]: fps = "<<float(frames)/(float(clock()-start_time)/CLOCKS_PER_SEC)<<", timestamp = "<<clock()/CLOCKS_PER_SEC<<std::endl;reset = true;}//fps
+    if(print_fps_)if(++frames>10){std::cerr<<"[ped_detector]: fps = "<<float(frames)/(float(clock()-start_time)/CLOCKS_PER_SEC)<<", timestamp = "<<clock()/CLOCKS_PER_SEC<<std::endl;reset = true;}//fps
 }
 
 int main(int argc, char **argv) {
